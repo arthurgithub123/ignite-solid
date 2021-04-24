3c680c14605baa5951f3ef7388b88a4565028664
Arthur <arthuromp000@gmail.com>
2021-04-24 13:40:10 -0300

    Add code for tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,20 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    
+    if (Array.isArray(user_id)) {
+      throw Error("User identifier (user_id) must be a string");
+    }
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+    
+      return response.json(users);
+    }
+    catch(err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
